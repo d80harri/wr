3cdab665ab2ed34ab17c1052dca35d8023ef345d
Harald Bittermann (HBI) <harald.bittermann@ntsretail.com>
2015-02-11 12:04:55 +0100

    some refactoring in treebinding

--- wr/ui/src/main/java/net/d80harri/wr/ui/takstree/binding/TaskTreeBinding.java
+++ wr/ui/src/main/java/net/d80harri/wr/ui/takstree/binding/TaskTreeBinding.java
@@ -2,52 +2,94 @@ package net.d80harri.wr.ui.takstree.binding;
 
 import static org.fxmisc.easybind.EasyBind.listBind;
 import static org.fxmisc.easybind.EasyBind.map;
-import net.d80harri.wr.ui.task.TaskPresentationModel;
+
+import org.fxmisc.easybind.EasyBind;
+
 import javafx.beans.binding.ObjectBinding;
 import javafx.beans.value.ChangeListener;
-import javafx.scene.control.TreeItem;import javafx.scene.control.TreeTableView;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeItem.TreeModificationEvent;
+import javafx.scene.control.TreeTableView;
+import net.d80harri.wr.ui.task.TaskPresentationModel;
 
+public class TaskTreeBinding extends
+		ObjectBinding<TreeItem<TaskPresentationModel>> {
 
-public class TaskTreeBinding extends ObjectBinding<TreeItem<TaskPresentationModel>>{
-	
 	private TaskPresentationModel model;
 	private TreeTableView<TaskPresentationModel> tree;
 	private TreeItem<TaskPresentationModel> computedItem;
-	
-	public TaskTreeBinding(TaskPresentationModel model, TreeTableView<TaskPresentationModel> tree) {
+
+	public TaskTreeBinding(TaskPresentationModel model,
+			TreeTableView<TaskPresentationModel> tree) {
 		this.model = model;
 		this.tree = tree;
-		
-//		bind(this.model.selectedProperty());
 	}
-	
+
 	@Override
 	protected TreeItem<TaskPresentationModel> computeValue() {
 		computedItem = new TreeItem<TaskPresentationModel>(this.model);
-		listBind(computedItem.getChildren(), map(model.getChildren(), i -> {
-			TreeItem<TaskPresentationModel> child = new TaskTreeBinding(i, tree).get();
-			
-			return child;
-		}));
-		
-		model.selectedProperty().addListener(listener);
-		
-		computedItem.addEventHandler(TreeItem.<TaskPresentationModel>childrenModificationEvent(), c -> {
-			if (c.wasRemoved()) {
-//				for (TreeItem<TaskPresentationModel> item : c.getRemovedChildren()) {
-//					item.getValue().selectedProperty().removeListener(listener);
-//				}
-				this.invalidate();
-			}
-			c.consume();
-		});
-		
+		listBind(
+				computedItem.getChildren(),
+				map(model.getChildren(),
+						i -> new TaskTreeBinding(i, tree).get()));
+
+		model.selectedProperty().addListener(selectedPropertyChangedListener);
+
+		computedItem.parentProperty().addListener(parentChangedListener);
+
 		return computedItem;
 	}
 
-	private final ChangeListener<? super Boolean> listener = (obs, o, n) -> {
-		if (n) {
-			tree.getSelectionModel().select(computedItem);
+	private final ChangeListener<? super Boolean> selectedPropertyChangedListener = new ChangeListener<Boolean>() {
+
+		@Override
+		public void changed(ObservableValue<? extends Boolean> observable,
+				Boolean oldValue, Boolean newValue) {
+			if (newValue) {
+				tree.getSelectionModel().select(computedItem);
+			}
+		}
+
+	};
+
+	private final EventHandler<TreeModificationEvent<TaskPresentationModel>> childModificationEventHanlder = new EventHandler<TreeItem.TreeModificationEvent<TaskPresentationModel>>() {
+
+		@Override
+		public void handle(TreeModificationEvent<TaskPresentationModel> event) {
+			if (event.wasRemoved()) {
+				for (TreeItem<TaskPresentationModel> item : event
+						.getRemovedChildren()) {
+					onTreeItemDeleted(event.getTreeItem(), item);
+				}
+			}
+			event.consume();
+		}
+
+	};
+
+	private final ChangeListener<? super TreeItem<TaskPresentationModel>> parentChangedListener = new ChangeListener<TreeItem<TaskPresentationModel>>() {
+
+		@Override
+		public void changed(
+				ObservableValue<? extends TreeItem<TaskPresentationModel>> observable,
+				TreeItem<TaskPresentationModel> oldValue,
+				TreeItem<TaskPresentationModel> newValue) {
+			if (oldValue == null)
+				return;
+			oldValue.addEventHandler(TreeItem
+					.<TaskPresentationModel> childrenModificationEvent(),
+					childModificationEventHanlder);
+			oldValue.parentProperty().removeListener(parentChangedListener);
 		}
 	};
+
+	private void onTreeItemDeleted(TreeItem<TaskPresentationModel> parent, TreeItem<TaskPresentationModel> deleted) {
+		deleted.parentProperty().removeListener(parentChangedListener);
+		deleted.getValue().selectedProperty().removeListener(selectedPropertyChangedListener);
+		parent.removeEventHandler(TreeItem
+					.<TaskPresentationModel> childrenModificationEvent(), childModificationEventHanlder);
+	}
+	
 }
